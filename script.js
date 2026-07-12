7cec8d68050bca80f41d6406908e05a06a8e77b9
jaylordlapena817-star <jaylordlapena817@gmail.com>
2026-07-12 23:43:32 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,89 @@
+const API = "https://YOUR-RENDER-APP.onrender.com";
+
+
+
+async function connectBot(){
+
+
+const host =
+document.getElementById("host").value;
+
+
+const port =
+document.getElementById("port").value;
+
+
+const amount =
+document.getElementById("amount").value;
+
+
+
+document.getElementById("status").innerHTML =
+"Connecting...";
+
+
+
+const response = await fetch(
+API + "/connect",
+{
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify({
+
+host:host,
+
+port:port,
+
+amount:Number(amount)
+
+})
+
+});
+
+
+
+const data = await response.json();
+
+
+
+document.getElementById("status").innerHTML =
+data.message;
+
+
+}
+
+
+
+
+
+async function stopBot(){
+
+
+document.getElementById("status").innerHTML =
+"Stopping...";
+
+
+
+const response = await fetch(
+API + "/stop",
+{
+method:"POST"
+}
+);
+
+
+
+const data = await response.json();
+
+
+
+document.getElementById("status").innerHTML =
+data.message;
+
+
+}
